0486ba06fa6db0f9aa59bdca7005061079387c17
Fery Affandi <feryaffandy2702@gmail.com>
2022-07-14 02:47:07 +0700

    1

---
+++ lab-uas-codeigniterv4/app/Views/index.php
@@ -0,0 +1,10 @@
+<?= $this->extend('/layouts/tempelate'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container bag">
+
+    <h1 style="color:white" >Halo, Ini halaman Utama Serangkai Cafe!</h1>
+    <p  style="color:white">Silahkan mengambil nomor antrian</p>
+     
+</div>
+<?= $this->endSection(); ?>
---
+++ lab-uas-codeigniterv4/public/htdocs/app/Views/index.php
@@ -0,0 +1,10 @@
+<?= $this->extend('/layouts/tempelate'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container bag">
+
+    <h1 style="color:black" >Halo, Ini halaman Utama Serangkai Cafe!</h1>
+    <p  style="color:black">Silahkan mengambil nomor antrian</p>
+     
+</div>
+<?= $this->endSection(); ?>
